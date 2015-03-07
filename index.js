6e7f8fe7b3927f6559a68dffd6604b4e76e41274
Nathan Rajlich <nathan@tootallnate.net>
2015-03-06 18:33:57 -0800

    big refactor to export an ES6 Iterator
    Now uses "node-iterator" module, instead of "dom-iterator".

--- index.js
+++ index.js
@@ -3,77 +3,43 @@
  * Module dependencies.
  */
 
-import DomIterator from 'dom-iterator';
+import NodeIterator from 'node-iterator';
 
 /**
- * A DOM Iterator that traverses between the start and end points
+ * Returns an ES6 Iterator that traverses between the start and end points
  * of a `Range` instance.
  *
- * @class
+ * @param {Range} range - Range instance to iterator over selected Nodes
+ * @param {Number} [whatToShow] - Bitwise OR'd list of Filter specification constants from the NodeFilter DOM interface
+ * @param {NodeFilter} [filter] - An object implementing the NodeFilter interface
+ * @param {Boolean} [entityReferenceExpansion] - A flag that specifies whether entity reference nodes are expanded
  * @public
  */
 
-class RangeIterator extends DomIterator {
-  constructor(range) {
-    if (!range) throw new TypeError('a Range instance is required!');
+function RangeIterator(range, whatToShow, filter, entityReferenceExpansion) {
+  if (!range) throw new TypeError('a Range instance must be given');
 
-    this.startContainer = range.startContainer;
-    this.endContainer = range.endContainer;
+  let startContainer = range.startContainer;
+  let endContainer = range.endContainer;
+  let start = range.commonAncestorContainer;
+  let doc = start.ownerDocument;
 
-    var start = range.commonAncestorContainer;
-
-    // if a TextNode is selected, then grab the text node's
-    // parent as the start node
-    if (start.nodeType === 3 /* TEXT_NODE */) {
-      start = start.parentNode;
-    }
-
-    super(start, null);
+  function acceptNode (node) {
+    if (!withinRange(node)) return NodeFilter.FILTER_REJECT;
+    if (!filter) return NodeFilter.FILTER_ACCEPT;
+    return filter(node);
   }
 
-  /**
-   * `selects()` override function to ensure that `node` is within the
-   * Range selection. We need to override instead of appending to the "selects"
-   * list because the list is a OR list, and in this case we want AND logic.
-   *
-   * @param {Node} node
-   * @param {Boolean} peek
-   * @return {Boolean}
-   * @public
-   */
-
-  selects(node, peek) {
-    return this.withinRange(node) && super.selects(node, peek);
-  }
-
-  /**
-   * Returns `true` when `node` is within the range's selection,
-   * returns `false` otherwise.
-   *
-   * @param {Node} node
-   * @return {Boolean}
-   * @public
-   */
-
-  withinRange(node) {
-    return node === this.startContainer ||
-      node === this.endContainer ||
-      (Boolean(node.compareDocumentPosition(this.startContainer) & Node.DOCUMENT_POSITION_PRECEDING)
+  function withinRange(node) {
+    return node === startContainer ||
+      node === endContainer ||
+      (Boolean(node.compareDocumentPosition(startContainer) & Node.DOCUMENT_POSITION_PRECEDING)
         &&
-      Boolean(node.compareDocumentPosition(this.endContainer) & Node.DOCUMENT_POSITION_FOLLOWING));
+      Boolean(node.compareDocumentPosition(endContainer) & Node.DOCUMENT_POSITION_FOLLOWING));
   }
+
+  return NodeIterator(start, whatToShow, acceptNode, entityReferenceExpansion);
 }
-
-/**
- * ES6 Iterable implementation.
- */
-
-RangeIterator.prototype[Symbol.iterator] = function *() {
-  let next;
-  while (next = this.next()) {
-    yield next;
-  }
-};
 
 /**
  * Module exports.
--- test/test.js
+++ test/test.js
@@ -1,12 +1,10 @@
 
-let assert = require('assert');
-let DomIterator = require('dom-iterator');
-let RangeIterator = require('../');
+var assert = require('assert');
+var RangeIterator = require('../');
 
 describe('RangeIterator', function () {
 
-  let d = document.createElement('div');
-
+  var d = document.createElement('div');
   d.style.display = 'none';
   document.body.appendChild(d);
 
@@ -28,29 +26,19 @@ describe('RangeIterator', function () {
     document.body.removeChild(d);
   });
 
-  it('should create a `RangeIterator` instance', function () {
-    let range = document.createRange();
+  it('should return an ES6 Iterator', function () {
+    var range = document.createRange();
     range.setStart(d.firstChild.firstChild, 2);
     range.setEnd(d.firstChild.firstChild, 2);
     assert(range.collapsed);
 
-    let iterator = new RangeIterator(range);
-    assert(iterator instanceof RangeIterator);
-  });
-
-  it('should be a subclass of `DomIterator`', function () {
-    let range = document.createRange();
-    range.setStart(d.firstChild.firstChild, 2);
-    range.setEnd(d.firstChild.firstChild, 2);
-    assert(range.collapsed);
-
-    let iterator = new RangeIterator(range);
-    assert(iterator instanceof DomIterator);
+    var iterator = RangeIterator(range);
+    assert.equal('function', typeof iterator.next);
   });
 
   it('should throw a TypeError if no Range is given', function () {
     try {
-      new RangeIterator(null);
+      RangeIterator(null);
       assert(false); // unreachable
     } catch (e) {
       assert(e);
@@ -59,188 +47,170 @@ describe('RangeIterator', function () {
   });
 
   it('should iterate over the TextNodes within a Range', function () {
-    let range = document.createRange();
+    var range = document.createRange();
     range.setStart(d.firstChild.firstChild, 2);
     range.setEnd(d.lastChild.firstChild, 2);
     assert.equal('llo world.fo', range.toString());
 
-    let next;
-    let iterator = new RangeIterator(range)
-      .select(3 /* Node.TEXT_NODE */);
+    var next;
+    var iterator = RangeIterator(range, NodeFilter.SHOW_TEXT);
 
     next = iterator.next();
-    assert(next.nodeValue === 'hello ');
+    assert.equal(false, next.done);
+    assert.equal(next.value.nodeValue, 'hello ');
 
     next = iterator.next();
-    assert(next.nodeValue === 'world');
+    assert.equal(false, next.done);
+    assert.equal(next.value.nodeValue, 'world');
 
     next = iterator.next();
-    assert(next.nodeValue === '.');
+    assert.equal(false, next.done);
+    assert.equal(next.value.nodeValue, '.');
 
     next = iterator.next();
-    assert(next.nodeValue === 'foo');
+    assert.equal(false, next.done);
+    assert.equal(next.value.nodeValue, 'foo');
 
     next = iterator.next();
-    assert(!next);
+    assert.equal(true, next.done);
   });
 
   it('should iterate over the TextNodes within a Range selecting beginning of last paragraph', function () {
-    let range = document.createRange();
+    var range = document.createRange();
     range.setStart(d.firstChild.firstChild, 0);
     range.setEnd(d.lastChild, 0);
     assert.equal('hello world.', range.toString());
 
-    let next;
-    let iterator = new RangeIterator(range)
-      .select(3 /* Node.TEXT_NODE */);
+    var next;
+    var iterator = RangeIterator(range, NodeFilter.SHOW_TEXT);
 
     next = iterator.next();
-    assert(next.nodeValue === 'hello ');
+    assert.equal(false, next.done);
+    assert.equal(next.value.nodeValue, 'hello ');
 
     next = iterator.next();
-    assert(next.nodeValue === 'world');
+    assert.equal(false, next.done);
+    assert.equal(next.value.nodeValue, 'world');
 
     next = iterator.next();
-    assert(next.nodeValue === '.');
+    assert.equal(false, next.done);
+    assert.equal(next.value.nodeValue, '.');
 
     next = iterator.next();
-    assert(!next);
+    assert.equal(true, next.done);
   });
 
   it('should iterate over the B element within a Range', function () {
-    let range = document.createRange();
+    var range = document.createRange();
     range.setStart(d.firstChild.firstChild, 2);
     range.setEnd(d.lastChild.firstChild, 2);
     assert.equal('llo world.fo', range.toString());
 
-    let next;
-    let iterator = new RangeIterator(range)
-      .revisit(false)
-      .select('nodeName === "B"');
+    var next;
+    var iterator = RangeIterator(range, NodeFilter.SHOW_ALL, function (node) {
+      return node.nodeName === 'B';
+    });
 
     next = iterator.next();
-    assert(next.nodeName === 'B');
-    assert(next.firstChild.nodeValue === 'world');
+    assert.equal(false, next.done);
+    assert.equal(next.value.nodeName, 'B');
+    assert.equal(next.value.firstChild.nodeValue, 'world');
 
     next = iterator.next();
-    assert(!next);
+    assert.equal(true, next.done);
   });
 
   it('should iterate over the TextNode within a collapsed Range', function () {
-    let range = document.createRange();
+    var range = document.createRange();
     range.setStart(d.lastChild.firstChild, 2);
     range.setEnd(d.lastChild.firstChild, 2);
     assert(range.collapsed);
 
-    let next;
-    let iterator = new RangeIterator(range)
-      .select(3 /* Node.TEXT_NODE */);
+    var next;
+    var iterator = RangeIterator(range, NodeFilter.SHOW_TEXT);
 
     next = iterator.next();
-    assert(next.nodeValue === 'foo');
+    assert.equal(false, next.done);
+    assert.equal(next.value.nodeValue, 'foo');
 
     next = iterator.next();
-    assert(!next);
+    assert.equal(true, next.done);
   });
 
   it('should iterate over the TextNodes and BR elements within a Range with multiple blocks', function () {
-    d.innerHTML = '<p>hel<b>lo</b></p><p><b><br></b></p><p><b><br></b></p><p><b>wo</b>rld</p>';
+    d.innerHTML = '<p>' +
+                    'hel' +
+                    '<b>lo</b>' +
+                  '</p>' +
+                  '<p>' +
+                    '<b><br></b>' +
+                  '</p>' +
+                  '<p>' +
+                    '<b><br></b>' +
+                  '</p>' +
+                  '<p>' +
+                    '<b>wo</b>' +
+                    'rld' +
+                  '</p>';
 
-    let range = document.createRange();
+    var range = document.createRange();
     range.setStart(d.firstChild.firstChild, 0);
     range.setEnd(d.lastChild.lastChild, 3);
     assert(!range.collapsed);
     assert.equal('helloworld', range.toString());
 
-    let next;
-    let iterator = new RangeIterator(range)
-      .revisit(false)
-      .select(3 /* Node.TEXT_NODE */)
-      .select(function (node) {
-        return 'BR' === node.nodeName;
-      });
+    var next;
+    var iterator = RangeIterator(range, NodeFilter.SHOW_TEXT | NodeFilter.SHOW_ELEMENT, function (node) {
+      return node.nodeType === Node.TEXT_NODE || 'BR' === node.nodeName;
+    });
 
     next = iterator.next();
-    assert(next.nodeValue === 'hel');
+    assert.equal(false, next.done);
+    assert(next.value.nodeValue, 'hel');
 
     next = iterator.next();
-    assert(next.nodeValue === 'lo');
+    assert.equal(false, next.done);
+    assert.equal(next.value.nodeValue, 'lo');
 
     next = iterator.next();
-    assert(next.nodeName === 'BR');
+    assert.equal(false, next.done);
+    assert.equal(next.value.nodeName, 'BR');
 
     next = iterator.next();
-    assert(next.nodeName === 'BR');
+    assert.equal(false, next.done);
+    assert.equal(next.value.nodeName, 'BR');
 
     next = iterator.next();
-    assert(next.nodeValue === 'wo');
+    assert.equal(false, next.done);
+    assert.equal(next.value.nodeValue, 'wo');
 
     next = iterator.next();
-    assert(next.nodeValue === 'rld');
+    assert.equal(false, next.done);
+    assert.equal(next.value.nodeValue, 'rld');
 
     next = iterator.next();
-    assert(!next);
+    assert.equal(true, next.done);
   });
 
   it('should iterate over the TextNodes within a Range that ends with the beginning of a P', function () {
-    d.innerHTML = '<p>foo</p><p>bar</p>';
+    d.innerHTML = '<p>foo</p>' +
+                  '<p>bar</p>';
 
-    let range = document.createRange();
+    var range = document.createRange();
     range.setStart(d.firstChild.firstChild, 0);
     range.setEnd(d.lastChild, 0);
     assert(!range.collapsed);
     assert.equal('foo', range.toString());
 
-    let next;
-    let iterator = new RangeIterator(range)
-      .revisit(false)
-      .select(3 /* Node.TEXT_NODE */);
+    var next;
+    var iterator = RangeIterator(range, NodeFilter.SHOW_TEXT);
 
     next = iterator.next();
-    assert(next.nodeValue === 'foo');
+    assert.equal(false, next.done);
+    assert.equal(next.value.nodeValue, 'foo');
 
     next = iterator.next();
-    assert(!next);
-  });
-
-  describe('@@iterator', function () {
-
-    it('should be an ES6 Iterable', function () {
-      d.innerHTML = '<p>one</p><p>two</p><p>three</p>';
-
-      let range = document.createRange();
-      range.setStart(d.firstChild.firstChild, 0);
-      range.setEnd(d.lastChild.firstChild, 5);
-      assert(!range.collapsed);
-      assert.equal('onetwothree', range.toString());
-
-      let iterator = new RangeIterator(range)
-        .revisit(false)
-        .select(3 /* Node.TEXT_NODE */);
-
-      // `Array.from()` and `[...iterator]` don't
-      // work in babelify for some reason :(
-      let count = 0;
-      for (let node of iterator) {
-        count++;
-        switch (count) {
-          case 1:
-            assert.equal('one', node.nodeValue);
-            continue;
-          case 2:
-            assert.equal('two', node.nodeValue);
-            continue;
-          case 3:
-            assert.equal('three', node.nodeValue);
-            continue;
-          default:
-            assert(0, 'should not happen');
-            continue;
-        }
-      }
-      assert.equal(3, count);
-    });
-
+    assert.equal(true, next.done);
   });
 
 });
